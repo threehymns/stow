92f5065195c4206f03184dbdd86f23b08a09b75a
threehymns <threehymns1@gmail.com>
2025-03-28 18:20:37 -0400

    fix: Don't rerender `HeaderActions` on note/folder creation

--- src/components/HeaderActions.tsx
+++ src/components/HeaderActions.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import useNoteStore from "@/store/noteStore";
 import { FolderPlus, Plus } from "lucide-react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
@@ -9,21 +10,27 @@ import { SidebarIcon } from "lucide-react";
 const HeaderActions = () => {
   const { createFolder, createNote } = useNoteStore();
   const sidebar = useSidebar();
-  const MotionButton = motion.create(Button);
-  const list = {
-    visible: { opacity: 1, width: "auto" },
-    hidden: { opacity: 0, width: 0 },
-  };
+  const MotionButton = React.useMemo(() => motion.create(Button), []);
+  const list = React.useMemo(
+    () => ({
+      visible: { opacity: 1, width: "auto" },
+      hidden: { opacity: 0, width: 0 },
+    }),
+    []
+  );
 
-  const item = {
-    visible: { opacity: 1, x: 0 },
-    hidden: { opacity: 0, x: -50 },
-  };
+  const item = React.useMemo(
+    () => ({
+      visible: { opacity: 1, x: 0 },
+      hidden: { opacity: 0, x: -50 },
+    }),
+    []
+  );
 
   return (
     <div
       className={cn(
-        "fixed top-2 left-2 z-[10000] bg-sidebar p-1 rounded-lg flex space-x-1",
+        "fixed top-2 left-2 z-[10000] bg-sidebar p-1 rounded-lg flex space-x-1 transition-colors duration-500",
         sidebar.open && "bg-transparent"
       )}
     >
@@ -36,35 +43,38 @@ const HeaderActions = () => {
       >
         <SidebarIcon />
       </Button>
-      {!sidebar.open && (
-        <motion.div
-          className="flex space-x-1"
-          initial="hidden"
-          whileInView="visible"
-          variants={list}
-        >
-          <MotionButton
-            onClick={() => createFolder("New Folder")}
-            variant="ghost"
-            size="icon"
-            className="size-7"
-            title="New Folder"
-            variants={item}
+      <AnimatePresence>
+        {!sidebar.open && (
+          <motion.div
+            className="flex space-x-1 -z-10"
+            initial="hidden"
+            whileInView="visible"
+            exit="hidden"
+            variants={list}
           >
-            <FolderPlus />
-          </MotionButton>
-          <MotionButton
-            onClick={() => createNote(null)}
-            variant="ghost"
-            size="icon"
-            className="size-7"
-            title="New Note"
-            variants={item}
-          >
-            <Plus />
-          </MotionButton>
-        </motion.div>
-      )}
+            <MotionButton
+              onClick={() => createFolder("New Folder")}
+              variant="ghost"
+              size="icon"
+              className="size-7"
+              title="New Folder"
+              variants={item}
+            >
+              <FolderPlus />
+            </MotionButton>
+            <MotionButton
+              onClick={() => createNote(null)}
+              variant="ghost"
+              size="icon"
+              className="size-7"
+              title="New Note"
+              variants={item}
+            >
+              <Plus />
+            </MotionButton>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
